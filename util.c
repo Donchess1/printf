14d29f08b006ca24e2d89eb2c72f38bbd9cbc16d
Donchess1 <Stepabod@yahoo.com>
2023-07-25 19:06:42 -0700

    moving

--- util.c
+++ util.c
@@ -6,9 +6,9 @@
  *
  * Return: 1 if c is printable, 0 otherwise
  */
-int is_printable(char c)
+int is_printable(char a)
 {
-	if (c >= 32 && c < 127)
+	if (a >= 32 && a < 127)
 		return (1);
 
 	return (0);
@@ -21,18 +21,18 @@ int is_printable(char c)
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-int append_hexa_code(char ascii_code, char buffer[], int i)
+int append_hexa_code(char ascii_code, char buffer[], int a)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
-	buffer[i++] = '\\';
-	buffer[i++] = 'x';
+	buffer[a++] = '\\';
+	buffer[a++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buffer[a++] = map_to[ascii_code / 16];
+	buffer[a] = map_to[ascii_code % 16];
 
 	return (3);
 }
@@ -43,9 +43,9 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
-int is_digit(char c)
+int is_digit(char b)
 {
-	if (c >= '0' && c <= '9')
+	if (b >= '0' && b <= '9')
 		return (1);
 
 	return (0);
